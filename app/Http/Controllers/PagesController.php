46dc747cdcceaad75c446d15db17f58518e4898f
cristinagiacobbe <crigiak@gmail.com>
2024-04-17 23:17:25 +0200

    trasnfer movies-db to the view

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
+    public function index()
+    {
+
+        $movies = Movie::all();
+        /* dd($movies); */
+
+        return view('welcome');
+    }
 }
